c292791d17b8a60291b364487f5e91a016723212
pepe13245 <jeniferbrito1325@gmail.com>
2026-09-06 14:20:31 -0400

    use signal computed to reflect updates on the page

--- panel-actividades/src/app/app.ts
+++ panel-actividades/src/app/app.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { TarjetaActividad } from './actividades/tarjeta-actividad/tarjeta-actividad';
 import { ResumenActividades } from './actividades/resumen-actividades/resumen-actividades';
 import { ListaActividades } from './actividades/lista-actividades/lista-actividades';
+import { TableroPrioridades } from './tablero-prioridades/tablero-prioridades';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TarjetaActividad, ResumenActividades, ListaActividades],
+  imports: [RouterOutlet, TarjetaActividad, ResumenActividades, ListaActividades, TableroPrioridades],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('panel-actividades');
 }
